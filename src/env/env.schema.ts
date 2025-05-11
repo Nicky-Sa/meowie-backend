50ca95d8e0fb3a49fd7f8e549bf80f6179f09a2a
nicky <n.sahraee@innovattic.com>
2025-05-12 00:04:42 +0200

    deploying, attempt 1 ;)

--- src/env/env.schema.ts
+++ src/env/env.schema.ts
@@ -4,7 +4,7 @@ export const envSchema = z.object({
   TMDB_API_KEY: z.string().min(1, { message: 'TMDB_API_KEY is required' }),
   OMDB_API_KEY: z.string().min(1, { message: 'OMDB_API_KEY is required' }),
   PORT: z.coerce.number({ message: 'PORT is required' }),
-  BUILD_ENV: z.enum(['local', 'development', 'preview', 'production'], {
+  BUILD_ENV: z.enum(['development', 'preview', 'production'], {
     message: 'BUILD_ENV is required',
   }),
 });
