1507be7603ff8d65fbbfa9388252897275e856b4
nicky <n.sahraee@innovattic.com>
2025-05-16 19:05:06 +0200

    responding with 200 and basic info for /

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getInfo(): Record<string, string> {
+    return {
+      app: 'meowie-backend',
+      version: '0.0.1',
+    };
   }
 }
