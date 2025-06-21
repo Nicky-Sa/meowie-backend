911c297ea7968dd50fc5b2da1860cb35cf6d5e0e
nicky <n.sahraee@innovattic.com>
2025-06-22 01:40:27 +0200

    removed api prefix

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,9 @@ import './instrument';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EnvService } from 'src/env/env.service';
-import { RequestMethod } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api', {
-    exclude: [{ path: '/', method: RequestMethod.GET }], // Exclude the root from the prefix
-  });
   app.enableCors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
