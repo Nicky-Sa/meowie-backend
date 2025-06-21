209dd9ea21bce6acf6ccad880c8373b66e75a6f3
nicky <n.sahraee@innovattic.com>
2025-06-22 01:18:40 +0200

    attempt 19 - buildenv as info data added

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import pkg from '../package.json';
+import { EnvService } from './env/env.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly envService: EnvService) {}
+
   getInfo(): Record<string, string> {
     return {
       app: pkg.name,
+      environment: this.envService.get('BUILD_ENV'),
       version: pkg.version,
     };
   }
