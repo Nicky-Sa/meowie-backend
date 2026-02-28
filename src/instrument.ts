e7750ef70a43a7a75aa5f79a0d0dcff12eb3a467
nicky <n.sahraee@innovattic.com>
2026-02-28 01:03:02 +0100

    disabled PII collection

--- src/instrument.ts
+++ src/instrument.ts
@@ -3,7 +3,6 @@ import * as Sentry from '@sentry/nestjs';
 Sentry.init({
   dsn: 'https://2b8af543cb4cf16e722afb265c4bebc2@o4509265002037248.ingest.de.sentry.io/4509272962039888',
   environment: process.env.BUILD_ENV,
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
-  sendDefaultPii: true,
+  // We explicitly disable PII data tracking to avoid IP and username collection
+  sendDefaultPii: false,
 });
