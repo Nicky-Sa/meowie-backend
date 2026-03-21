d43750109eb374dc80ee70ed6df2f1d711abb9c3
nicky <n.sahraee@innovattic.com>
2026-03-21 14:21:44 +0100

    default blurhash updated

--- src/common/app.constants.ts
+++ src/common/app.constants.ts
@@ -8,4 +8,4 @@ export const TV_POSTER_FALLBACK_URL =
 export const PERSON_FALLBACK_URL =
   'https://meowie-public.s3.eu-central-1.amazonaws.com/person-fallback.png';
 export const LIMIT = 20;
-export const DEFAULT_BLURHASH = 'U11o;?of00of00of00of00of00of00of00of';
+export const DEFAULT_BLURHASH = 'L18z.G~qRkxakWofofkCD%RjNGj[';
